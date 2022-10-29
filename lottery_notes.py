bc97bddf5560c515a679197dc2adb5c00bfb3cb7
“hirotada tamura <tamura0425@gmail.com>
2022-10-29 13:05:14 +0900

    Python クラス作成

--- lottery_notes.py
+++ lottery_notes.py
@@ -2,58 +2,72 @@ import openpyxl
 import logging
 import csv
 import pprint
-
 from openpyxl import load_workbook
 from openpyxl import Workbook
 import glob
 
 
-def create_books():
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.title = 'test_sheet_1'
-    # シートを追加
-    wb.create_sheet()
-    wb.create_sheet(index=1,title="555555")
-    
-    # 保存する
-    wb.save('C:\\Users\\tamur\\Desktop\\lottery_notes\\test2.xlsx')
-    glob.glob("*.xlsx")
+class SimpleData:
+# """A simple example class"""         # 三重クォートによるコメント
 
-def create_book():
-    wb = Workbook()
-    # ワークブックの読み込み\
-    wb.save('C:\\Users\\tamur\\Desktop\\lottery_notes\\myworkbook.xlsx')
-    # ワークシートの選択
-    ws = wb['Sheet']  # ワークシートを指定
-    ws = wb.active  # アクティブなワークシートを選択
-    print(f'sheet name: {ws.title}')  # sheet name: Sheet
-    for sheet in wb:
-        print(f'sheet name: {sheet.title}')
+    def __init__(self):                  # コンストラクタ
+        self.name = ""
+
+    def create_books(self):
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.title = 'test_sheet_1'
+        # シートを追加
+        wb.create_sheet()
+        wb.create_sheet(index=1,title="555555")
+
+        # 保存する
+        wb.save('C:\\Users\\tamur\\Desktop\\lottery_notes\\test2.xlsx')
+        glob.glob("*.xlsx")
 
 
-# ブックデータ取得メソッド
-def lottery_notes():
-    # ブックを取得
-    get_book = openpyxl.load_workbook('C:\\Users\\tamur\\Desktop\\lottery_notes\\lottery_notes.xlsx')
-    # シートを取得 
-    get_seat = get_book["Sheet1"]
-    # セルを１つずつ取得
-    for cell1 in tuple(get_seat.rows):
-        for cell2 in cell1:
-            print(cell2.value)
+    def create_book(self):
+        wb = Workbook()
+        # ワークブックの読み込み\
+        wb.save('C:\\Users\\tamur\\Desktop\\lottery_notes\\myworkbook.xlsx')
+        # ワークシートの選択
+        ws = wb['Sheet']  # ワークシートを指定
+        ws = wb.active  # アクティブなワークシートを選択
+        print(f'sheet name: {ws.title}')  # sheet name: Sheet
+        for sheet in wb:
+            print(f'sheet name: {sheet.title}')
 
 
-# error logging作成
-def create_log():
-    logging.basicConfig()
-    logger = logging.getLogger(__name__)
-    logger.error("エラーが発生しました")
+    # ブックデータ取得メソッド
+    def lottery_notes(self):
+        # ブックを取得
+        get_book = openpyxl.load_workbook('C:\\Users\\tamur\\Desktop\\lottery_notes\\lottery_notes.xlsx')
+        # シートを取得
+        get_seat = get_book["Sheet1"]
+        # セルを１つずつ取得
+        for cell1 in tuple(get_seat.rows):
+            for cell2 in cell1:
+                print(cell2.value)
+
+    # error logging作成
+    def create_log(self):
+        logging.basicConfig()
+        logger = logging.getLogger(__name__)
+        logger.error("エラーが発生しました")
 
 
-create_log()
+    def hello(self):
+        print("hello world55555")
 
-lottery_notes()
+ # クラスのインスタンスを生成
+a =  SimpleData()
+a.create_log()
 
-create_books()
-create_book()
+a.lottery_notes()
+
+a.create_books()
+a.create_book()
+
+a.hello()
+a.hello()
+a.hello()
