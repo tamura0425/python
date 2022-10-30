ba89e051230e6d0fb6077f4c4600fa10fd0e95ea
“hirotada tamura <tamura0425@gmail.com>
2022-10-30 23:14:01 +0900

    スクレイピング完了

--- lottery_notes.py
+++ lottery_notes.py
@@ -6,8 +6,12 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 import glob
 
+import requests
+from bs4 import BeautifulSoup
 
-class SimpleData:
+
+
+class LotteryDataAcquisition:
 # """A simple example class"""         # 三重クォートによるコメント
 
     def __init__(self):                  # コンストラクタ
@@ -20,11 +24,9 @@ class SimpleData:
         # シートを追加
         wb.create_sheet()
         wb.create_sheet(index=1,title="555555")
-
         # 保存する
         wb.save('C:\\Users\\tamur\\Desktop\\lottery_notes\\test2.xlsx')
         glob.glob("*.xlsx")
-
 
     def create_book(self):
         wb = Workbook()
@@ -36,7 +38,6 @@ class SimpleData:
         print(f'sheet name: {ws.title}')  # sheet name: Sheet
         for sheet in wb:
             print(f'sheet name: {sheet.title}')
-
 
     # ブックデータ取得メソッド
     def lottery_notes(self):
@@ -55,19 +56,32 @@ class SimpleData:
         logger = logging.getLogger(__name__)
         logger.error("エラーが発生しました")
 
-
     def hello(self):
         print("hello world55555")
 
+
+
+    def scraping(self):
+        TARGET_URL = "http://www.ohtashp.com/topics/takarakuji/index_loto6.html" # スクレイピングしたいURLをここに書く
+        html = requests.get(TARGET_URL)  # HTMLを取ってくる
+        soup = BeautifulSoup(html.content, "html.parser")  # HTMLを解析する
+        title = soup.find("title")  # データを抽出する
+        print(title)
+
+
+
+
  # クラスのインスタンスを生成
-a =  SimpleData()
-a.create_log()
+rotteryData =  LotteryDataAcquisition()
+rotteryData.create_log()
 
-a.lottery_notes()
+rotteryData.lottery_notes()
 
-a.create_books()
-a.create_book()
+rotteryData.create_books()
+rotteryData.create_book()
 
-a.hello()
-a.hello()
-a.hello()
+rotteryData.hello()
+
+rotteryData.scraping()
+
+rotteryData.hello()
