d0b64278494a8161edfe9078fcd28dbc3c820d48
“hirotada tamura <tamura0425@gmail.com>
2023-03-26 17:14:29 +0900

    Update Image.py

--- Image.py
+++ Image.py
@@ -17,21 +17,19 @@ logging.basicConfig(filename='example.log', level=logging.ERROR)
 # jpg ファイルを読み込み、PDF ファイルに変換します
 def convert_to_pdf(input_folder, output_file, dpi):
     try:
-        	mages = []
-                    	for filename in sorted(os.:%dlistdir(input_folder)):
-                                    	if filename.endswith(".JPG"):
-                                                        	filepath = os.path.join(input_folder, filename)
-                                                                                img = Image.open(filepath)
-                                                                                	images.append(img)                                                                                                                                                                                                                                                                                                                                               # 画像をPDFにまとめます
-                                                                                    if images:
-                                                                                    	images[0].save(output_file, save_all=True, append_images=images[1:], dpi=(dpi,dpi))
+       	mages = []
+       	for filename in sorted(os.:%dlistdir(input_folder)):
+            if filename.endswith(".JPG"):
+                filepath = os.path.join(input_folder, filename)
+                img = Image.open(filepath)
+                images.append(img)                                                                                                                                                                                                                                                                                                                                               # 画像をPDFにまとめます
+    if images:
+       images[0].save(output_file, save_all=True, append_images=images[1:], dpi=(dpi,dpi))
                                                                                         except Exception as e:
                                                                                         	logging.error(e)
-
-                                                                                                try:
-                                                                                                	convert_to_pdf(input_folder, output_file, dpi)
-                                                                                                    except Exception as e:
-                                                                                                    	print(e)
-                                                                                                        	logging.error(e)
+try:
+                                                                                                convert_to_pdf(input_folder, output_file, dpi)
+            except Exception as e:
+            print(e)logging.error(e)
 
 
